992d6909dc771af38bef7fed30ce73918637e752
biranchikulesika <biranchikulesika@gmail.com>
2024-10-24 05:13:27 +0530

    added circle.c

---
+++ Assignment 1/circle.c
@@ -0,0 +1,26 @@
+/*A C program to calculate the area and circumference of a circle based on the radius entered by the user.
+
+File: Circle.c
+Author: Biranchi Kulesika
+Date: 2 Sep, 2023
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{    
+		double radius, circumference, area;
+
+    printf("\n\nEnter the radius of the circle: ");
+    scanf("%lf", &radius);
+    
+    circumference = 2*M_PI*radius;
+    area = M_PI*radius*radius;
+    
+    printf("\n\nRadius: %.2lf units", radius);    
+    printf("\nCircumference: %.2lf units", circumference);    
+    printf("\nArea: %.2lf square units\n\n", area);
+    
+    return 0;
+}
